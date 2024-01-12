8cbf8c991e779cf22ee06b6b5d8bd53bd4ff9c2e
Saurabh Shrikhande <saurabhshrikhande@200gmail.com>
2024-01-13 03:04:31 +0530

    remain resolve issue of total

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import './App.css';
 import Productmap from './productmap';
+import { useState } from 'react';
 
 function App() {
-   
+   let [updatedarr , setarr] = useState([]);
+   let [total , settotal] = useState(0);
+
+   function cartfun (uparr){
+    console.log(uparr)  
+    // setarr(uparr);
+    function abc(){
+      setarr(() =>  updatedarr = uparr);
+    }
+    abc();
+    let tot = 0;
+       updatedarr.map((item) => {
+         tot = tot + parseInt(item.price) * parseInt(item.initval);
+         settotal(() => total = tot);
+    })
+   }
 
   return (
     <div style={{display:"flex" , gap: "25px", margin:"5vh 8vw" }}>
         
      <div style={{width:"50vw" , textAlign:"center", border:"10px solid gray" , padding:"10px"}}>
         <h1 >Products</h1>
-           <Productmap/>
+           <Productmap cart = {cartfun}  />
      </div>
 
       <div style={{width:"34vw" , textAlign:"center", border:"10px solid gray" , padding:"10px"}}>
+           
             <h1>Cart</h1>
+
+            <div>
+              {
+                updatedarr.map((item, idx) => {
+                  return <div key = {idx}>
+                  {
+                  item.initval !== 0  && <div style={{ display: "flex", color: "white", justifyContent: "space-between", padding: "20px 15px", backgroundColor: "#a6a6a6", margin: "20px 10px"}}>
+                    <h3>{item.name} </h3>
+                    <h3>{item.price} X {item.initval}</h3>
+                    </div> 
+                  }   
+                  </div>
+                })
+              }
+            </div>
+
+            <div style={{ display: "flex", color: "white", justifyContent: "space-between", padding: "20px 15px", backgroundColor: "#a6a6a6", margin: "20px 10px"}}>
+               <h1>Total:</h1>
+               <h1>{total}</h1>
+            </div>
       </div>
 
     </div>
--- src/productmap.js
+++ src/productmap.js
@@ -1,50 +1,62 @@
 import { useState } from "react";
 
-export default function Productmap (){
-   
-   
-   let [Products , setprodinit] = useState(
-        [
-        {id: 1, name: "Product-1", price:100 , initval:0},
-        {id: 2, name: "Product-2", price:200 , initval:0},
-        {id: 3, name: "Product-3", price:300 , initval:0},
-        ]
-   );
-    function minus(key){
-      const item =  Products.map((product) =>{
-            if(product.id === key && product.initval > 0){
-               return { ...product , initval : product.initval + 1};
-            }
-        })
-        return item;
-    }
-    function plus(key){
-        
-    }
-   
+export default function Productmap(props) {
 
-    return (
-        <div>
-            {
-        Products.map((product , key) =>{          
-          return( <div key={product.id}>
-                <div style={{display:"flex", color:"white",justifyContent:"space-between", padding:"20px 15px", backgroundColor:"#a6a6a6", margin:"20px 10px"}}>
-                 <h3>{product.name}</h3>
-                 <h3>{product.price}</h3>
-                    <div style={{display:"flex", backgroundColor:"#3B93A9", padding:"0px 10px", borderRadius:"50px"}}>
-                       <button style={{padding:"0px 20px", backgroundColor:"#3B93A9", border:"none", color:"white", fontSize:"35px", borderRadius:"50px"}}
-                       onClick={() =>{ minus(key)} }>-</button>
-                       <h3 style={{margin:"auto 10px"}}>{product.initval}</h3>
-                       <button style={{padding:"0px 20px", backgroundColor:"#3B93A9", border:"none", color:"white", fontSize:"25px", borderRadius:"50px"}}
-                       onClick={() =>{ plus(key)}}>+</button>
-                    </div>
-               </div>
-   
-             </div> )
-           } ) 
-          }
+
+  let [Products, setprodinit] = useState(
+    [
+      { id: 1, name: "Product-1", price: 100, initval: 0 },
+      { id: 2, name: "Product-2", price: 200, initval: 0 },
+      { id: 3, name: "Product-3", price: 300, initval: 0 },
+    ]
+  );
+  function minus(key) {
+    const updatedArr = Products.map((product) => {
+      if (product.id === key && product.initval !== 0 ) {
+        return { ...product, initval: product.initval - 1 };
+      }
+      return product;
+    })
+    //        return item;
+    setprodinit(updatedArr);
+    props.cart(updatedArr);
+
+  }
+  function plus(key) {
+    const updatedArr = Products.map((product) => {
+      if (product.id === key) {
+        return { ...product, initval: product.initval + 1 };
+      }
+      return product;
+    })
+    //        return item;
+    setprodinit(updatedArr);
+    props.cart(updatedArr);
+  }
+
+
+  return (
+    <div>
+      {
+        Products.map((product) => {
+          return (<div key={product.id}>
+            <div style={{ display: "flex", color: "white", justifyContent: "space-between", padding: "20px 15px", backgroundColor: "#a6a6a6", margin: "20px 10px" }}>
+              <h3>{product.name}</h3>
+              <h3>{product.price}</h3>
+              <div style={{ display: "flex", backgroundColor: "#3B93A9", padding: "0px 10px", borderRadius: "50px" }}>
+                <button style={{ padding: "0px 20px", backgroundColor: "#3B93A9", border: "none", color: "white", fontSize: "35px", borderRadius: "50px" }}
+                  onClick={() => minus(product.id)}>-</button>
+                <h3 style={{ margin: "auto 10px" }}>{product.initval}</h3>
+                <button style={{ padding: "0px 20px", backgroundColor: "#3B93A9", border: "none", color: "white", fontSize: "25px", borderRadius: "50px" }}
+                  onClick={() => { plus(product.id)}}>+</button>
+              </div>
             </div>
-         
-    )
-       
+
+          </div>)
+        })
+      }
+    </div>
+
+  )
+
 }
